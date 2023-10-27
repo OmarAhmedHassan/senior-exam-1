bd13b7acc841562b5975ce78bfb6eb90e0512af0
legend <omar@gt4p.com>
2023-10-27 16:49:12 +0200

    Saving progress

---
+++ draft/draft.php
@@ -0,0 +1,51 @@
+<?php   
+/*
+$x =['a','b','c'];
+$x[]=['d'];
+$x[3]='a';
+
+$x =10 + + '10'+ -'10' - -100 /10;
+echo $x;
+var_dump( 100 <=> 11 ) ;
+
+$x=true;
+
+$y= $x == true ? 'sen':'stp';
+
+
+
+$x=1;
+
+while ($x < 3) { 
+    if($x == 2) { continue; }
+    echo $x;
+    $x++ ;   
+
+}
+
+*/
+
+function x(): ?int {  
+return null;
+}
+
+var_dump(x()) ;
+
+
+
+/*
+$employes=
+[
+    'ahmed' => ['id' => '100','name' => '100_m4'
+    ],[
+    'id' => '200',
+    'phone' => ['id' => '100','work' => '987987']
+    ],
+];
+
+$your_num=$employes[0]['phone']['work'];
+
+var_dump($employes);*/
+?>
+
+
